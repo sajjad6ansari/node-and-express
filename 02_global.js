1c418ea9e429380932fb5e2fd6e6ed446efe5122
sajjad6ansari <sajjad6ansari@gmail.com>
2024-05-03 22:03:28 +0530

    globally available functions in Node.JS

---
+++ 02_global.js
@@ -0,0 +1,15 @@
+// console.log(__dirname);
+// C:\Users\SAJJAD ANSARI\Desktop\javascript learning\webcodes\node-and-expresss
+
+
+// console.log(__filename);
+// C:\Users\SAJJAD ANSARI\Desktop\javascript learning\webcodes\node-and-expresss\app.js
+
+
+setInterval(()=>{
+    console.log("Hello World");
+},1000);
+
+setTimeout(()=>{
+    console.log("My name is Sajjad !!!!");
+},5000);
